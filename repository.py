bb618809cd6deba88dcc064f49260f137ed00151
joanna-goncalves <joanna.goncalvesp@gmail.com>
2025-02-07 16:50:48 +0100

    n/A

--- repository.py
+++ repository.py
@@ -7,12 +7,11 @@ end_date = "2009-12-31"
 
 def get_data():
     return pd.read_csv(
-        r"input\crisis_portfolio.csv",
+        r"input\small_portfolio.csv",
         delimiter=",",
-        index_col="Date",
-        parse_dates=["Date"],
+        index_col="date",
+        parse_dates=["date"],
     )
-
 
 def get_weights() -> np.array:
     return np.array(list(portfolio.values()))
