4f1528410f924ea028990e31835d8dcd3c3f236c
joanna-goncalves <joanna.goncalvesp@gmail.com>
2025-02-12 15:29:14 +0100

    filtre du dataframe avec les dates de départ et de fin

--- repository.py
+++ repository.py
@@ -6,12 +6,15 @@ begin_date = "2008-01-01"
 end_date = "2009-12-31"
 
 def get_data():
-    return pd.read_csv(
+    df = pd.read_csv(
         r"input\crisis_portfolio.csv",
         delimiter=",",
         index_col="Date",
         parse_dates=["Date"],
     )
+    df = df.loc["2008-01-01":"2009-12-31"]
+    return df
+
 
 def get_weights() -> np.array:
     return np.array(list(portfolio.values()))
