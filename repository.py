7503d4d1bc62799318a93425368e16327903d2e2
joanna-goncalves <joanna.goncalvesp@gmail.com>
2025-02-12 15:37:15 +0100

    correction repository

--- repository.py
+++ repository.py
@@ -2,8 +2,7 @@ import pandas as pd
 import numpy as np
 
 portfolio = {"Citi": 0.25, "MS": 0.25, "GS": 0.25, "JPM": 0.25}
-begin_date = "2008-01-01"
-end_date = "2009-12-31"
+
 
 def get_data():
     df = pd.read_csv(
